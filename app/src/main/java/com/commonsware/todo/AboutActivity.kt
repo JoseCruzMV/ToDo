e80c5b2f21d2d6ddc75885aea0bbe290893b0ae8
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-04-05 13:41:46 -0500

    Created About activity, for the AboutOption item, and shows an html defined as an asset

---
+++ app/src/main/java/com/commonsware/todo/AboutActivity.kt
@@ -0,0 +1,17 @@
+package com.commonsware.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.commonsware.todo.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityAboutBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.toolbar.title = getString(R.string.app_name)
+        binding.about.loadUrl("file:///android_asset/about.html")
+    }
+}
--- app/src/main/java/com/commonsware/todo/MainActivity.kt
+++ app/src/main/java/com/commonsware/todo/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.commonsware.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.commonsware.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
 
-    setContentView(binding.root)
-    setSupportActionBar(binding.toolbar)
-  }
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
 
-  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-    menuInflater.inflate(R.menu.actions, menu)
-    return super.onCreateOptionsMenu(menu)
-  }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actions, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about -> {
+                startActivity(Intent(this, AboutActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
