d472072409a53b17cff6bf826f3e8bc7716d2716
Jose C Morgado Vargas <josecruzmv@outlook.com>
2022-04-05 19:09:21 -0500

    Created ToDoRepository class with dummy data

---
+++ app/src/main/java/com/commonsware/todo/ToDoRepository.kt
@@ -0,0 +1,18 @@
+package com.commonsware.todo
+
+class ToDoRepository {
+    var items = listOf(
+        ToDoModel(
+            description = "Buy a copy of _Exploring Android_",
+            isCompleted = true,
+            notes = "See https://wares.commonsware.com"
+        ),
+        ToDoModel(
+            description = "Complete all of the tutorials"
+        ),
+        ToDoModel(
+            description = "Write an app for somebody in my community",
+            notes = "Talk to some people at non-profit organizations to see what they need!"
+        )
+    )
+}
